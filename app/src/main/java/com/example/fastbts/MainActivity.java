1f025953528f1443680e7d266543c8902a3c3810
xingyao.li <xingyao.li@nio.com>
2021-12-21 15:00:49 +0800

    delete ndk tools

--- app/src/main/java/com/example/fastbts/MainActivity.java
+++ app/src/main/java/com/example/fastbts/MainActivity.java
@@ -3,56 +3,17 @@ package com.example.fastbts;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import 	android.net.wifi.ScanResult;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.os.Message;
-import android.telecom.TelecomManager;
-import android.telephony.CellIdentity;
-import android.telephony.CellIdentityCdma;
-import android.telephony.CellIdentityGsm;
-import android.telephony.CellIdentityLte;
-import android.telephony.CellIdentityNr;
-import android.telephony.CellIdentityTdscdma;
-import android.telephony.CellIdentityWcdma;
-import android.telephony.CellInfo;
-import android.telephony.CellInfoCdma;
-import android.telephony.CellInfoGsm;
-import android.telephony.CellInfoLte;
-import android.telephony.CellInfoNr;
-import android.telephony.CellInfoTdscdma;
-import android.telephony.CellInfoWcdma;
-import android.telephony.CellSignalStrength;
-import android.telephony.CellSignalStrengthCdma;
-import android.telephony.CellSignalStrengthGsm;
-import android.telephony.CellSignalStrengthLte;
-import android.telephony.CellSignalStrengthNr;
-import android.telephony.CellSignalStrengthTdscdma;
-import android.telephony.CellSignalStrengthWcdma;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import com.google.gson.Gson;
 
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -88,7 +49,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.text);
-        textView.setText(NDKTools.stringFromJNI());
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(this);
--- app/src/main/java/com/example/fastbts/NDKTools.java
+++
@@ -1,9 +0,0 @@
-package com.example.fastbts;
-
-public class NDKTools {
-    public static native String stringFromJNI();
-    public static native double[] CIS(long[] speedList, int size);
-    static {
-        System.loadLibrary("fastbts");
-    }
-}
